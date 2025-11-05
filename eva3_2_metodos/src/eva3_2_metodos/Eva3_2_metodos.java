32ddebdb509392ac568f250caeccd406c9c3d7c9
Nicol1104 <L25550909@chihuahua2.tecnm.mx>
2025-11-05 17:54:28 -0600

    Add files via upload

---
+++ eva3_2_metodos/src/eva3_2_metodos/Eva3_2_metodos.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_2_metodos;
+
+/**
+ *
+ * @author nikol
+ */
+public class Eva3_2_metodos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        repetirMensaje("Hola",5);//primero es stric y luego int ,no regresa ningun valor 
+    }
+    //declaracion de metodo 
+    public static void repetirMensaje(String mensaje,int repeticion){
+            for(int i=1; i<=repeticion;i++){
+                System.out.println("mensaje");
+            
+            }
+}
+}
